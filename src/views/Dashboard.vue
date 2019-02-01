4dde5d90e9e129b806369d27be16816b7d4fe3c2
Aleksander Giera <aleksander.giera@protonmail.com>
2019-02-01 21:07:52 +0100

    fake data for dashboard and search

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,13 +3,41 @@
         <h1 class="subheading">Dasboard</h1>
         
         <v-container class="my-5">
-            <p>lorem</p>
+            <v-card flat v-for="item in items" :key="item.type">
+                <v-layout row wrap class="pa-3">
+                    <v-flex xs12 md6>
+                        <div class="caption grey--text">Device Type</div>
+                        <div>{{ item.type }}</div>
+                    </v-flex>
+                    <v-flex xs6 sm4 md3>
+                        <div class="caption grey--text">Number of devices</div>
+                        <div>{{ item.number }}</div>
+                    </v-flex>
+                    <v-flex xs3 sm2 md1>
+                        <v-btn>View</v-btn>
+                    </v-flex>
+                    <v-flex xs3 sm2 md1>
+                        <v-btn>Add</v-btn>
+                    </v-flex>
+                    <v-flex xs3 sm2 md1>
+                        <v-btn>Delete</v-btn>
+                    </v-flex>
+                </v-layout>
+            </v-card>
         </v-container>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            items: [
+                {type: 'Switch', number: 12},
+                {type: 'Router', number: 3},
+                {type: 'PatchPanel', number: 40},
+            ]
+        };
+    },
 }
 </script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,7 +3,42 @@
         <h1 class="subheading">Search</h1>
         
         <v-container class="my-5">
-            <p>lorem</p>
+            <v-card flat xs12 md4 lg3 class="pa-3">
+               <v-form>
+                    <v-select
+                       :items="deviceTypes"
+                       label="Device Types"
+                       v-model="devType"
+                    ></v-select>
+                    <v-text-field
+                        v-model="searchName"
+                        label="Search for ID (whatever it can be)"
+                    >
+                    </v-text-field>
+                    <v-btn
+                        color="primary"
+                        @click="searchForData"
+                    >Search</v-btn>
+               </v-form>
+            </v-card>
         </v-container>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            devType: " ",
+            searchName: " ",
+            deviceTypes: ['Router', 'Switch', 'PatchPanel', 'Room Socket', 'Server','Printer', 'Access Point']
+
+        }
+    },
+    methods: {
+        searchForData(){
+            console.log("search for "+this.devType+" with ID "+this.searchName)
+        }    
+    }
+}
+</script>
+

6ecd4da0a6768cf08ea912a3feb9baa81dcf4d5e
Aleksander Giera <aleksander.giera@protonmail.com>
2019-09-01 22:06:27 +0200

    added vlans to setting screen

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,16 +2,18 @@
   <div class="add">
     <v-container class="my-5">
       <PortSpeedList/>
+      <VlanNamesList/>
     </v-container>
   </div>
 </template>
 
 <script>
 import PortSpeedList from "@/components/settings/PortSpeedList.vue";
-
+import VlanNamesList from "@/components/settings/VlanNamesList.vue";
 export default {
   components: {
-    PortSpeedList
+    PortSpeedList,
+    VlanNamesList
   }
 };
 </script>
